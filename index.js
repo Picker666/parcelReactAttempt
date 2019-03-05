18185fb5bd00adba6ab305ca6894f7fa22b4e356
任军峰 <renjf@shinemo.com>
2019-03-05 18:41:49 +0800

    first commit.

---
+++ index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+
+class HelloWorld extends Component {
+    constructor () {
+        super()
+    }
+
+    render () {
+        const { name } = this.props;
+        return <div className="content">{`Hello, ${name} ！！！`}</div>
+    }
+}
+
+const mountNode = document.getElementById("root");
+ReactDOM.render(<HelloWorld name="Picker" />, mountNode);
